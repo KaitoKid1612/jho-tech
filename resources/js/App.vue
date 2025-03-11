cc950d921f7eef706a613b20931b2f518b915733
kaito <leviet1612@gmail.com>
2025-03-11 23:43:02 +0700

    feat: config tailwind

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        <nav>
+    <div class="d-flex size-6">
+        <nav class="d-flex">
             <router-link to="/">Home</router-link> |
             <router-link to="/contacts">Contacts</router-link> |
             <router-link to="/login">Login</router-link>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel + Vue 3</title>
-    @vite('resources/js/app.js')
+    @vite(["resources/js/app.js", "resources/css/app.css"])
 </head>
-<body>
+<body class="bg-gray-100 text-gray-900">
     <div id="app"></div>
 </body>
 </html>
